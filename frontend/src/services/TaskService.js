1c20905a3e09f605a820b7ab81158eb6f61a5fce
Jayden Cheung <100813722+1jaydencc@users.noreply.github.com>
2024-04-25 18:43:13 -0400

    Update TaskService.js

--- frontend/src/services/TaskService.js
+++ frontend/src/services/TaskService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: `https://34.31.207.186:3000`,
+    baseURL: `https://34.31.207.186:443`,
     withCredentials: false, // This is the default
     headers: {
         Accept: "application/json",
