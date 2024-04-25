d6547ab383212e5ca28536d700381ac8137dcf9c
1jaydencc <cheung69@purdue.edu>
2024-04-24 23:59:41 -0400

    changed server address

--- frontend/src/services/TaskService.js
+++ frontend/src/services/TaskService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: `http://localhost:3000`,
+    baseURL: `http://146.235.209.208:3000`,
     withCredentials: false, // This is the default
     headers: {
         Accept: "application/json",
